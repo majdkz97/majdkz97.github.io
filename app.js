ca6a63d24ab98f24fc8f626effd5c142c05fd739
MajdKZ <>
2022-11-22 12:10:56 +0300

    Updates

--- app.js
+++ app.js
@@ -1,66 +1,59 @@
-let ch=document.getElementById('switch');
-let programing=document.querySelectorAll("#programing");
-let design=document.querySelectorAll('#design');
-let right=document.querySelector('.right');
-let left =document.querySelector('.left');
+let programingCards=document.querySelectorAll("#programing");
+let designCards=document.querySelectorAll('#design');
+let switchProgramming=document.querySelector('.switch-programming');
+let switchDesign =document.querySelector('.switch-design');
+let previousIndex = 0; //Default Active section is programming
 
-// for(let i=0;i<programing.length;i++)
-// {
-  
-//   programing[i].style.display='none';
 
-// }
-left.style.border='10px solid';
-  right.style.border='1px solid';
-  for(let i=0;i<design.length;i++)
+switchProgramming.style.border='10px solid';
+switchDesign.style.border='1px solid';
+  for(let i=0;i<programingCards.length;i++)
   {
-    design[i].style.display='block';
+    programingCards[i].style.display='block';
     
   
   }
-  for(let i=0;i<programing.length;i++)
+  for(let i=0;i<designCards.length;i++)
   {
     
-    programing[i].style.display='none';
+    designCards[i].style.display='none';
   
   }
 
-ch.checked=false;
-function myfuck()
+function onChangeSection(selectedIndex)
 {
-if(ch.checked)
-{
-  right.style.border='10px solid';
-  left.style.border='1px solid';
-  for(let i=0;i<design.length;i++)
+if(previousIndex!=selectedIndex)
+ {
+  previousIndex = selectedIndex;
+  if(selectedIndex==0)
   {
-    design[i].style.display='block';
-    
-  
+    switchProgramming.style.border='10px solid';
+    switchDesign.style.border='1px solid';
+    for(let i=0;i<designCards.length;i++)
+    {
+      designCards[i].style.display='none';
+    }
+    for(let i=0;i<programingCards.length;i++)
+    {
+      programingCards[i].style.display='block';
+    }
   }
-  for(let i=0;i<programing.length;i++)
+  else if(selectedIndex == 1)
   {
-    
-    programing[i].style.display='none';
-  
+    switchProgramming.style.border='1px solid';
+    switchDesign.style.border='10px solid';
+    for(let i=0;i<designCards.length;i++)
+    {
+      designCards[i].style.display='block';
+    }
+    for(let i=0;i<programingCards.length;i++)
+    {
+      programingCards[i].style.display='none';
+    }
   }
-}
-else {
-  left.style.border='10px solid';
-  right.style.border='1px solid';
 
-  for(let i=0;i<design.length;i++)
-  {
-    design[i].style.display='none';
-    
+ 
   
-  }
-  for(let i=0;i<programing.length;i++)
-  {
-    
-    programing[i].style.display='block';
-  
-  }
 }}
 
 
